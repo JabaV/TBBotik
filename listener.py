8e40c90ce6ae7884af4b7402fb5b895e66da3ce2
Virus-33 <51784979+Virus-33@users.noreply.github.com>
2023-03-09 11:52:49 +0700

    Deleted RKASS until it is done

--- listener.py
+++ listener.py
@@ -3,11 +3,10 @@ import os
 import vk_api
 from modules import message_handler
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
-from modules import RKASS_module
 secter = os.environ['token']
 
 keep_alive()
-vk_session = vk_api.VkApi(token=str(secter))  #
+vk_session = vk_api.VkApi(token=str(secter))
 longpoll = VkBotLongPoll(vk_session, 206394583)
 vk = vk_session.get_api()
 
@@ -21,16 +20,6 @@ for event in longpoll.listen():
                 message_handler.handle_replied(msg, r_text, chat_id, vk_session)
             else:
                 message_handler.handle_message(msg, chat_id, vk_session)
-        if event.from_user:
-            msg = event.object.message['text']
-            sender = event.object.message['peer_id']
-            RKASS_module.menu(sender, vk_session)
-            if msg == 'Проверить':
-                if (event.type == VkBotEventType.MESSAGE_NEW) & event.from_user:
-                    xt = event.object.message['text']
-                    ud = event.object.message['from_id']
-                    RKASS_module.inspection_user_send(xt, ud)
-
 
 
     # elif (event.type == VkBotEventType.WALL_POST_NEW) & (event.obj['post_type'] == 'post'):
