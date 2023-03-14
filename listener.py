8a1d26000d0954c56a2d927cd8102b4b844debaa
Virus-33 <51784979+Virus-33@users.noreply.github.com>
2023-03-14 21:05:13 +0700

    ENDLESS POWEEEEER

--- listener.py
+++ listener.py
@@ -11,17 +11,18 @@ vk_session = vk_api.VkApi(token=str(secter))
 longpoll = VkBotLongPoll(vk_session, 206394583)
 vk = vk_session.get_api()
 
-try:
-    for event in longpoll.listen():
-        if event.type == VkBotEventType.MESSAGE_NEW:
-            if event.from_chat:
-                chat_id = event.object.message['peer_id']
-                msg = event.object.message['text']
-                if 'reply_message' in event.object.message:
-                    r_text = event.object.message['reply_message']['text']
-                    message_handler.handle_replied(msg, r_text, chat_id, vk_session)
-                else:
-                    message_handler.handle_message(msg, chat_id, vk_session)
-        # elif (event.type == VkBotEventType.WALL_POST_NEW) & (event.obj['post_type'] == 'post'):
-except Exception as e:
-    module_logger.Log(e)
+while True:
+    try:
+        for event in longpoll.listen():
+            if event.type == VkBotEventType.MESSAGE_NEW:
+                if event.from_chat:
+                    chat_id = event.object.message['peer_id']
+                    msg = event.object.message['text']
+                    if 'reply_message' in event.object.message:
+                        r_text = event.object.message['reply_message']['text']
+                        message_handler.handle_replied(msg, r_text, chat_id, vk_session)
+                    else:
+                        message_handler.handle_message(msg, chat_id, vk_session)
+            # elif (event.type == VkBotEventType.WALL_POST_NEW) & (event.obj['post_type'] == 'post'):
+    except Exception as e:
+        module_logger.Log(e)
