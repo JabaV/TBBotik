609fc77bd3d05ab470d262d96543ae1fc1469e86
Virus-33 <51784979+Virus-33@users.noreply.github.com>
2023-04-01 16:24:40 +0700

    New handling out of if-else construction

--- modules/module_logger.py
+++ modules/module_logger.py
@@ -4,5 +4,5 @@ log = open("error_log.txt", "a+", encoding='utf-8')
 
 
 def Log(error):
-    log.write("[" + str(datetime.datetime.now().date()) + "]" + str(error) + "\n")
+    log.write("[" + str(datetime.datetime.now().date()) + "] " + str(error) + "\n")
     log.flush()
