20dc95fd08a88d6696a4f2a257ee92c373148f7b
Virus-33 <51784979+Virus-33@users.noreply.github.com>
2023-04-01 16:28:43 +0700

    Some fixes

--- modules/message_handler.py
+++ modules/message_handler.py
@@ -3,5 +3,6 @@ from modules import module_commands
 
 def handle_message(text, chat_id, vk_session, replied_text=""):
     if text[:2].lower() in module_commands.commands:
-        module_commands.commands[text[:2]](text, chat_id, vk_session, replied_text)
+        print(text[:2].lower())
+        module_commands.commands[text[:2].lower()](text, chat_id, vk_session, replied_text)
 
