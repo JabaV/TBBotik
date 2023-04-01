c4b20919fe99d3a9fb44d6b15ec57d3cf7abc2e0
Danil K <51784979+Virus-33@users.noreply.github.com>
2023-04-01 09:45:28 +0000

    Removed debug message

--- modules/message_handler.py
+++ modules/message_handler.py
@@ -3,6 +3,5 @@ from modules import module_commands
 
 def handle_message(text, chat_id, vk_session, replied_text=""):
     if text[:3].lower() in module_commands.commands:
-        print(text[:3].lower())
         module_commands.commands[text[:3].lower()](text, chat_id, vk_session, replied_text)
 
