de334c9dcbc3a5316704577de26bccdb6ae369d6
JabaV <88829870+JabaV@users.noreply.github.com>
2023-04-01 16:44:38 +0300

    Add files via upload

--- listener.py
+++ listener.py
@@ -2,27 +2,34 @@ from webserver import keep_alive
 import os
 import vk_api
 from modules import message_handler
-from modules import module_logger
 from vk_api.bot_longpoll import VkBotLongPoll, VkBotEventType
+from modules import RKASS_module
 secter = os.environ['token']
 
 keep_alive()
-vk_session = vk_api.VkApi(token=str(secter))
+vk_session = vk_api.VkApi(token=str(secter))  #
 longpoll = VkBotLongPoll(vk_session, 206394583)
 vk = vk_session.get_api()
 
-while True:
-    try:
-        for event in longpoll.listen():
-            if event.type == VkBotEventType.MESSAGE_NEW:
-                if event.from_chat:
-                    chat_id = event.object.message['peer_id']
-                    msg = event.object.message['text']
+for event in longpoll.listen():
+    if event.type == VkBotEventType.MESSAGE_NEW:
+        if event.from_chat:
+            chat_id = event.object.message['peer_id']
+            msg = event.object.message['text']
+            if 'reply_message' in event.object.message:
+                r_text = event.object.message['reply_message']['text']
+                message_handler.handle_replied(msg, r_text, chat_id, vk_session)
+            else:
+                message_handler.handle_message(msg, chat_id, vk_session)
+        if event.from_user:
+            msg = event.object.message['text']
+            sender = event.object.message['peer_id']
+            RKASS_module.menu(sender, vk_session)
+            if msg == 'Проверить':
+            xt = event.object.message['text']
+            ud = event.object.message['from_id']
+            RKASS_module.inspection_user_send(xt, ud)
 
-                    r_text = ""
-                    if 'reply_message' in event.object.message: 
-                        r_text = event.object.message['reply_message']['text']
-                    message_handler.handle_message(msg, chat_id, vk_session, r_text)
-            # elif (event.type == VkBotEventType.WALL_POST_NEW) & (event.obj['post_type'] == 'post'):
-    except Exception as e:
-        module_logger.Log(e)
+
+
+    # elif (event.type == VkBotEventType.WALL_POST_NEW) & (event.obj['post_type'] == 'post'):
